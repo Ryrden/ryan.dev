5fd7c5aba389377dbacc0fda013138edc67df1cc
Ryan S <ryanssteles@gmail.com>
2024-07-15 11:44:22 -0300

    feat: menu hamburguer navbar

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,6 @@
 function AddEventListenerToCvButton() {
     const curriculumLink = "https://drive.google.com/file/d/19pqHEiFgfk3KVx_rZrWjyfyNAXi1a0jI/view?usp=sharing";
     const cvButton = document.getElementById("cv")
-    cvButton.style.cursor = "pointer";
-    cvButton.addEventListener("mouseenter", () => { cvButton.innerText = "\u2197" });
-    cvButton.addEventListener("mouseleave", () => { cvButton.innerText = "Cv" })
     cvButton.addEventListener("click", () => window.open(curriculumLink, '_blank'))
 }
 
