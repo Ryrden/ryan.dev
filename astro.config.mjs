1e1cb78674c995c21eda38dbd4b519c16f266588
Ryan S <ryanssteles@gmail.com>
2025-04-16 19:09:06 -0300

    analytics: added ga

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,20 @@
-import { defineConfig } from "astro/config"
 import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
-import tailwind from "@astrojs/tailwind"
 import solidJs from "@astrojs/solid-js"
+import tailwind from "@astrojs/tailwind"
+import { defineConfig } from "astro/config"
+import partytown from "@astrojs/partytown"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://ryan.dev.br",
-  integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
+  integrations: [
+    mdx(), 
+    sitemap(),
+    solidJs(), 
+    tailwind({ applyBaseStyles: false }), 
+    partytown({ config: { forward: ['dataLayer.push'] } })
+  ],
   // TODO: Add support do Mermaid into blog markdown
   // READ: https://dev.to/fkurz/extending-astrojs-markdown-processing-with-remark-and-rehype-plugins-m1k
 })
